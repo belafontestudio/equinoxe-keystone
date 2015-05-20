5749b9cfd9cee6ab914032ba04f4773f1fdce88f
Piermaria Cosina <piermaria@belafonte.co>
2015-05-20 18:26:57 +0200

    fixes

--- routes/views/yachtCharter.js
+++ routes/views/yachtCharter.js
@@ -8,7 +8,33 @@ exports = module.exports = function(req, res) {
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'yacht_charter';
+	locals.data = {
+		featured: []
+	};
 	
+	// Load the posts
+	view.on('init', function(next) {
+		
+		var q = keystone.list('Yacht').paginate({
+				page: 1,
+				perPage: 3,
+				maxPages: 1
+			})
+			.where('state', 'published')
+			.sort('-publishedDate')
+			.where('availability').equals("Charter")
+			.where('featured',true);
+
+
+		
+		
+		q.exec(function(err, results) {
+			locals.data.featured = results;
+			console.log(locals.data.featured.results);
+			next(err);
+		});
+		
+	});
 	// Render the view
 	view.render('yacht_charter');
 	
